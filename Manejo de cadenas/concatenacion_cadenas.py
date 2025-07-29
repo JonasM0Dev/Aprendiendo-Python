e966462cb86f095ca7ef76edf4ed1d779716ad70
jonas mena <jonasmenadev@gmail.com>
2025-07-29 04:22:01 -0400

    Se agrega el proyecto

---
+++ Manejo de cadenas/concatenacion_cadenas.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+#Concatenacion de cadenas
+
+cadena1 = "Hola"
+cadena2 = "Mundo"
+
+concatenacion = cadena1 + " " + cadena2
+print (concatenacion)
+
+#Utilizando el metodo Join
+
+concatenacion = "".join([cadena1, " ", cadena2])
+print (concatenacion)
